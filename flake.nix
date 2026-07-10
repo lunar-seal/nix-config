71729f8bce4758752e7cc18ab44f60713287baa5
lunar-seal <lunar-seal@users.noreply.github.com>
2026-07-10 16:46:06 +0200

    chore: update lanzaboote

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     import-tree.url = "github:vic/import-tree";
     home-manager.url = "github:nix-community/home-manager/master";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    lanzaboote.url = "github:nix-community/lanzaboote/v0.4.3";
+    lanzaboote.url = "github:nix-community/lanzaboote/v1.1.0";
     lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
     nix-index-database.url = "github:nix-community/nix-index-database";
     nix-index-database.inputs.nixpkgs.follows = "nixpkgs";
